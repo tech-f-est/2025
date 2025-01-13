e744cbec2269cbcad992646df5bf06cab5c0cf44
Valentin Manceaux-Panot <valentin@mcxp.io>
2025-01-13 10:41:55 +0100

    Added some content to the landing page

--- src/pages/index.css.ts
+++ src/pages/index.css.ts
@@ -2,23 +2,9 @@ import { style } from "@vanilla-extract/css";
 import { theme } from "../theme.css";
 
 export const page = style({
-  display: "grid",
-  gridTemplateColumns: "1fr 60vw 1fr",
-  gridTemplateRows: "1fr 30vh 1fr",
-
-  alignItems: "center",
-
-  flexGrow: 1,
-
-  "@media": {
-    "(max-width: 768px)": {
-      gridTemplateColumns: `${theme.spacing.medium} auto ${theme.spacing.medium}`,
-      gridTemplateRows: `${theme.spacing.medium} auto ${theme.spacing.medium}`,
-
-      alignItems: "center",
-      justifyItems: "center",
-    },
-  },
+  scrollSnapType: "both mandatory",
+  overscrollBehaviorX: "contain",
+  overflow: "auto",
 });
 
 export const illustration = style({
@@ -26,25 +12,66 @@ export const illustration = style({
   gridRow: "2",
 });
 
-export const details = style({
-  display: "flex",
-  flexDirection: "column",
-  gap: theme.spacing.medium,
+export const section = style({
+  scrollSnapAlign: "center center",
+  scrollSnapStop: "normal",
+  height: "calc(100vh - 88px)",
+  width: "100vw",
 
+  display: "grid",
+  gridTemplateColumns: "1fr 60vw 1fr",
+  gridTemplateRows: "1fr 30vh 1fr",
+
+  alignItems: "center",
+
+  "@media": {
+    "(max-width: 768px)": {
+      gridTemplateColumns: `${theme.spacing.large} auto ${theme.spacing.large}`,
+      gridTemplateRows: `${theme.spacing.large} auto ${theme.spacing.large}`,
+
+      alignItems: "center",
+      justifyItems: "center",
+    },
+  },
+});
+
+export const hero = style([section]);
+
+export const sectionContent = style({
   gridColumn: "2",
   gridRow: "2",
 
   fontSize: "2rem",
-
-  textAlign: "right",
+  textWrap: "balance",
 
   "@media": {
     "(max-width: 768px)": {
-      fontSize: "1rem",
-      justifySelf: "end",
+      fontSize: "1.2rem",
     },
   },
+
+  textAlign: "center",
 });
+
+export const details = style([
+  sectionContent,
+  {
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing.medium,
+
+    fontSize: "2rem",
+
+    textAlign: "right",
+
+    "@media": {
+      "(max-width: 768px)": {
+        fontSize: "1rem",
+        justifySelf: "end",
+      },
+    },
+  },
+]);
 
 export const title = style({});
 
