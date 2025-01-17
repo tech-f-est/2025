ef54331a504f8d8a392b0ca075283903af100813
Siufong <siufong.h@gmail.com>
2025-01-17 20:14:41 +0100

    prettier, remove unnecessary styling

--- src/pages/association.css.ts
+++ src/pages/association.css.ts
@@ -5,13 +5,8 @@ export const page = style({
   maxWidth: `min(1280px, 100vw - calc(2 * ${theme.spacing.large}))`,
   alignSelf: "center",
 
-  marginTop: 200,
-
+  height: "100%",
   display: "flex",
   flexDirection: "column",
-  gap: theme.spacing.large,
-
-  "@media": {
-    "(max-width: 768px)": { marginTop: 0 },
-  },
+  justifyContent: "center",
 });
