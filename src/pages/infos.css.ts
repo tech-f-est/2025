f6ecbc530d142b7250671af0b5e9fa9a7f6891e3
Valentin Manceaux-Panot <Valentin Manceaux-Panot>
2025-02-02 18:20:19 +0100

    Improved the style of the information page

--- src/pages/infos.css.ts
+++ src/pages/infos.css.ts
@@ -1,17 +1,32 @@
-import { style } from "@vanilla-extract/css";
+import { globalStyle, style } from "@vanilla-extract/css";
 import { theme } from "../theme.css";
 
 export const page = style({
   maxWidth: `min(1280px, 100vw - calc(2 * ${theme.spacing.large}))`,
   alignSelf: "center",
 
-  marginTop: 200,
+  paddingBottom: theme.spacing.large,
 
   display: "flex",
   flexDirection: "column",
-  gap: theme.spacing.large,
+});
 
-  "@media": {
-    "(max-width: 768px)": { marginTop: 0 },
-  },
+globalStyle(`${page} h2`, {
+  marginTop: theme.spacing.medium,
+  marginBottom: theme.spacing.small,
+});
+globalStyle(`${page} h2:first-child`, {
+  marginTop: 0,
+});
+
+globalStyle(`${page} h3`, {
+  marginTop: theme.spacing.medium,
+  marginBottom: theme.spacing.small,
+});
+globalStyle(`${page} h2 + h3`, {
+  marginTop: 0,
+});
+
+globalStyle(`${page} p`, {
+  margin: 0,
 });
